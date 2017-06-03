f4a235659bf5b3731a38093381dae492d8d30ec4
Dmitry Zvorykin <dmitry.zvorikin@gmail.com>
2017-06-03 12:32:08 +0300

    animated boxes

--- 20170603-boxes/ViewController.m
+++ 20170603-boxes/ViewController.m
@@ -8,7 +8,16 @@
 
 #import "ViewController.h"
 
+NSTimeInterval animationDuration = 1;
+CGPoint blackBoxCenterStored;
+
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIButton *button1;
+@property (weak, nonatomic) IBOutlet UIButton *button2;
+@property (weak, nonatomic) IBOutlet UIView *blackBox;
+@property (weak, nonatomic) IBOutlet UIButton *button3;
+@property (weak, nonatomic) IBOutlet UIButton *button4;
 
 @end
 
@@ -16,14 +25,41 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    blackBoxCenterStored = self.blackBox.center;
 }
 
+- (IBAction)button1Click:(id)sender {    
+    [UIView animateWithDuration:animationDuration animations:^{
+        self.blackBox.alpha = 0;
+    }];
+}
+
+- (IBAction)button2Click:(id)sender {
+    [UIView animateWithDuration:animationDuration animations:^{
+        self.blackBox.alpha = 1;
+    }];
+}
+
+- (IBAction)button3Click:(id)sender {
+    CGAffineTransform transform = self.blackBox.transform;
+    [UIView animateWithDuration:animationDuration animations:^{
+        self.blackBox.transform = CGAffineTransformRotate(transform, M_PI / 2);
+        self.blackBox.center = blackBoxCenterStored;
+    }];    
+}
+
+- (IBAction)button4Click:(id)sender {
+    CGPoint blackBoxCenter = self.blackBox.center;
+    blackBoxCenter.y += 500;
+    [UIView animateWithDuration:animationDuration animations:^{
+        self.blackBox.center = blackBoxCenter;
+    }];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 
 @end
